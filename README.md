1191982ef1a75ba71b3732f8c949ec3a28cfbd0e
cerebralcourt <0hhvxhv9@anonaddy.me>
2021-03-09 15:31:11 -0800

    Movement

---
+++ README.md
@@ -0,0 +1,3 @@
+# gloss-snake
+
+A snake game made with Haskell using the graphics library [Gloss](https://hackage.haskell.org/package/gloss-1.8.1.2).
--- src/Main.hs
+++ src/Main.hs
@@ -7,11 +7,15 @@ import Debug.Trace
 
 -- Data Structures
 
+data Direction = NORTH | EAST | SOUTH | WEST
+
 data World = World
   { screenDims :: (Int, Int)
   , size :: (Int, Int)
   , snake :: [(Int, Int)]
   , apple :: (Int, Int)
+  , dir :: Direction
+  , step :: Float
   , seed :: StdGen
   }
 
@@ -32,11 +36,43 @@ makeWorld screenDims size seed =
     , size = size
     , snake = [snake]
     , apple = apple
+    , dir =
+        if snd snake < snd size `div` 2 then
+          EAST
+        else
+          WEST
+    , step = 0
     , seed = seed''
     }
 
+nextSnake :: [(Int, Int)] -> Direction -> [(Int, Int)]
+nextSnake snake dir =
+  let
+    head = nextHead (snake !! 0) dir
+    tail = [snake !! i | i <- [0..length snake - 2]]
+  in
+  head : tail
+  where
+    nextHead (i, j) NORTH = (i, j - 1)
+    nextHead (i, j) EAST = (i + 1, j)
+    nextHead (i, j) SOUTH = (i, j + 1)
+    nextHead (i, j) WEST = (i - 1, j)
+
 nextWorld :: Float -> World -> World
-nextWorld _ world = world
+nextWorld dt world =
+  let
+    newSnake =
+      if step world > 1 then
+        nextSnake (snake world) (dir world)
+      else
+        snake world
+    newStep =
+      if step world > 1 then
+        step world + dt - 1
+      else
+        step world + dt
+  in
+  world {snake = newSnake, step = newStep}
 
 -- Drawing
 
@@ -66,4 +102,4 @@ main = do
   let screenDims = (500, 500)
   let world = makeWorld screenDims (10, 10) seed
   let window = InWindow "Snake" screenDims (100, 100)
-  play window black 60 world drawWorld handleEvents nextWorld
+  play window black 20 world drawWorld handleEvents nextWorld
